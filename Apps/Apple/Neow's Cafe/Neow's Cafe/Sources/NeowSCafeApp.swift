df93714084371977e8396d9d03563c5decb537ec
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2026-04-24 20:34:15 +0100

    chore(cafe): init neow's cafe (iOS app) project with tuist

---
+++ Apps/Apple/Neow's Cafe/Neow's Cafe/Sources/NeowSCafeApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct NeowSCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apps/Apple/Neow's Cafe/Project.swift
@@ -0,0 +1,37 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Neow's Cafe",
+    targets: [
+        .target(
+            name: "Neow's Cafe",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.clooney.apple.Neow-s-Cafe",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            buildableFolders: [
+                "Neow's Cafe/Sources",
+                "Neow's Cafe/Resources",
+            ],
+            dependencies: []
+        ),
+        .target(
+            name: "Neow's CafeTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.clooney.apple.Neow-s-CafeTests",
+            infoPlist: .default,
+            buildableFolders: [
+                "Neow's Cafe/Tests"
+            ],
+            dependencies: [.target(name: "Neow's Cafe")]
+        ),
+    ]
+)
